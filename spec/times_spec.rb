cfd78353767cb79158fca45a3172b0964d7d6e32
Romena Murgor <romenamurgor@gmail.com>
2020-01-29 05:30:59 +0000

    Automatically backed up by Learn

--- spec/times_spec.rb
+++ spec/times_spec.rb
@@ -1,7 +1,7 @@
 require_relative "spec_helper"
 require_relative "../times.rb"
 
-describe "#using_times" do 
+describe "using_times" do 
 	it "puts out the phrase 'Wingardium Leviosa' 7 times by using the `times` construct" do
 
 	looping_string = "Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\n"
